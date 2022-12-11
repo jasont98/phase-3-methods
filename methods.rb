1deab1ab982b55e7df41e05c7c2f58c6f3c923f4
Hoang Tran <jashoang2583@gmail.com>
2022-12-10 19:13:50 -0800

    commit tree 04f3b3b4d61b4c8b731cc8db1a27c8941b64f3b2

--- methods.rb
+++ methods.rb
@@ -1,1 +1,12 @@
 # Your code here!
+def my_method param  
+    # puts 'Running myFunction'
+    puts param
+end
+
+puts my_method 'Running myFunction'
+
+def my_method param
+    puts 'Running my_method'
+    param + 1
+end
